12a631819e583584a4294d71dfd0ac15b08b4936
hassanssaade <hassanssaade@gmail.com>
2020-07-30 18:43:07 -0300

    added show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # First a route to list all tasks
+  get '/tasks', to: 'tasks#index'
+  # Create a route to check specific task
+  get '/tasks/:id', to: 'tasks#show'
 end
